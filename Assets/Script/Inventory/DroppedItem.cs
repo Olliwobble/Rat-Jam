c48091b58ef2bccfb74a83924983b0d77ce27e64
sorsaper <zlayutka@gmail.com>
2026-09-22 13:53:34 +0300

    dont touch a sword

---
+++ Assets/Script/Inventory/DroppedItem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DroppedItem : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField]
+    bool autoStart;
+    
+    [SerializeField]
+    float enabledPickupDelay = 3.0f;
+
+    [Header("Settings")]
+    public Item item;
+    public bool pickedUp = false;
+
+    private void Start()
+    {
+        if (autoStart && item != null)
+        {
+            Initialize(item);
+        }
+
+    }
+    public void Initialize(Item item)
+    {
+        this.item = item;
+        var droppedItem = Instantiate(item.prefab, transform);
+        droppedItem.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        StartCoroutine(EnablePickup(enabledPickupDelay));
+    }
+    IEnumerator EnablePickup(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GetComponent<Collider>().enabled = true;
+    }
+    
+    }
